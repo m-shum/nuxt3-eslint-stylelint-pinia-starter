6d47dab99f408904662777abcf1e94dada7866c4
Michael Shumakov <michael.shumakov@gmail.com>
2024-03-14 15:09:10 -0700

    feat: add style files & basic dir structure
    feat: add style files & basic dir structure
    
    feat: add style files & basic dir structure

---
+++ layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+  <main>
+    <Suspense>
+      <slot />
+      <template #fallback>
+        <div>Your fallback content here</div>
+      </template>
+    </Suspense>
+  </main>
+</template>
---
+++ pages/[...slug].vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <ExampleComponent />
+  </div>
+</template>
